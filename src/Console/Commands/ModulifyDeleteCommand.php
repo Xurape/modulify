2b94f6dee897b20f5fb077f739b68d0575995052
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-06-28 00:02:43 +0100

    fix: check and try to add debugging

--- src/Console/Commands/ModulifyDeleteCommand.php
+++ src/Console/Commands/ModulifyDeleteCommand.php
@@ -47,6 +47,8 @@ final class ModulifyDeleteCommand extends Command
 
         $this->info("Deleting module {$name}...");
 
+        $this->error(app_path("Modules/{$name}"));
+
         $this->deleteModule($name, app_path("Modules/{$name}"));
         $this->unregisterModule($name);
         
@@ -70,12 +72,12 @@ final class ModulifyDeleteCommand extends Command
             return true;
         }
 
-        if(!$this->files->isDirectory(app_path("Modules/{$name}"))) {
-            $this->error("Module {$name} does not exist.");
+        if (!File::exists(app_path("Modules/{$name}"))) {
+            $this->info("Module {$name} does not exist.");
             return true;
         }
 
-        if (!strpos(File::get(config_path('app.php')), "App\\Modules\\{$name}\\Providers\\{$name}ServiceProvider::class,") !== false) {
+        if (strpos(File::get(config_path('app.php')), "App\\Modules\\{$name}\\Providers\\{$name}ServiceProvider::class,") === false) {
             $this->info("Module {$name} is not registered.");
             return true;
         }
@@ -86,6 +88,7 @@ final class ModulifyDeleteCommand extends Command
     protected function deleteModule($name, $path)
     {
         $this->files->deleteDirectory($path);
+        File::delete(app_path("Modules/{$name}.php"));
     }
 
     protected function unregisterModule($name)
