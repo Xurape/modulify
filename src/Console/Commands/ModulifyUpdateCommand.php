94d909ed2b063822e68fd720beacc0cbd3867633
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-06-28 13:59:53 +0100

    add: update command scaffold

---
+++ src/Console/Commands/ModulifyUpdateCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xurape\Modulify\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use function Laravel\Prompts\spin;
+
+final class ModulifyUpdateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = "modulify:update";
+
+    /**
+     * The description of the console command.
+     *
+     * @var string
+     */
+    protected $description = "Update modulify ✨";
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->info("\n");
+        // print version in 
+        $this->info("\n");
+    }
+}
